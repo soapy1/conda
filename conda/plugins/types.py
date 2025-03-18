acc186b511030efbd0447b41b72c83ac410ee6f9
sophia <scastellarin@quansight.com>
2025-03-18 15:44:42 -0700

    Decouple install from env and cli arguments

--- conda/plugins/types.py
+++ conda/plugins/types.py
@@ -29,8 +29,6 @@ if TYPE_CHECKING:
             self,
             prefix: str,
             specs: Iterable[str],
-            args: Namespace,
-            env: Environment,
             *_,
             **kwargs,
         ) -> Iterable[str]: ...
@@ -39,8 +37,6 @@ if TYPE_CHECKING:
         def __call__(
             self,
             specs: Iterable[str],
-            args: Namespace,
-            env: Environment,
             *_,
             **kwargs,
         ) -> Environment: ...
