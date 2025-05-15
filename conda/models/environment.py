b59202cce0849012d6a03d36a4c3766a5178becf
sophia <scastellarin@quansight.com>
2025-05-15 10:41:18 -0700

    Sort in alphabetical order + add platform info

--- conda/models/environment.py
+++ conda/models/environment.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass, field
 from typing import Any
 from .match_spec import MatchSpec
 from .records import PackageRecord
+from ..base.context import context
 
 
 @dataclass
@@ -21,25 +22,29 @@ class Environment:
         self.configuration = configuration or {}
         self.variables = variables or {}
 
+    # Merged configuration for the environment.
+    configuration: dict[str, Any] | None = field(default_factory=dict)
+
+    # Map of other package types that conda can install. For example pypi packages.
+    # TODO: not sure if this is an ok way to capture this information
+    external_packages: dict[str, list]  | None = None
+
     # Environment name. One name or prefix is required.
     name: str |  None = None
 
     # Prefix the environment is installed into. One name or prefix is required.
     prefix: str | None = None
 
-    # User requested specs for this environment.
-    specs: list[MatchSpec] | None = None
-
-    # Map of other package types that conda can install. For example pypi packages.
-    # TODO: not sure if this is an ok way to capture this information
-    external_packages: dict[str, list]  | None = None
+    # The platform this environment may be installed on. Defaults to the 
+    # current platform.
+    platform: str = context.subdir
 
     # The complete list of specs for the environment.
     # eg. after a solve, or from an explicit environemnt spec
     records: list[PackageRecord] | None = None
 
-    # Merged configuration for the environment.
-    configuration: dict[str, Any] | None = field(default_factory=dict)
+    # User requested specs for this environment.
+    specs: list[MatchSpec] | None = None
 
     # Environment variables to be applied to the environment.
     variables: dict[str, str] | None = None
